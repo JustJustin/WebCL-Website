7128a936e9a70d7f23d9fd1f3b9b02c7a81ff301
JustJustin <egliju@onid.orst.edu>
2014-03-16 05:43:17 -0700

    Updated to fetch info.js for code.

--- assets/js/tutorials/tutorial01/info.js
+++ assets/js/tutorials/tutorial01/info.js
@@ -1,23 +1,43 @@
 var tutorial_data = 
 {
-    name:"Introduction",
-    lessons: [
+    name:"Vector Operations",
+    steps:
+    [   
         {
-            name:"Vector Operations",
-            steps:
-            [
-                { text: "Hi, for your first step try changing the addition to subtraction.",
-                  completion: function( code, user, output ) {
-                      var reg = /A\[x\] = B\[x\] + C\[x\];/gi;
-                      if( reg.exec(code) )
-                      {
-                          return true;
-                      }
-                      return false;
-                  }
+            text:"Write [code]print('hello');[/code]",
+            editor:0,
+            editorText:'/js/tutorials/tutorial01/start.js',
+            otherText:  {'1':'/js/tutorials/tutorial01/start.c' },
+            complete: function(code, user, output) {
+                var reg = /print\(['"]hello['"]\);/gi;
+                if( reg.exec( code ) ) {
+                    return true;
                 }
-            ]
+            },
+            lock: false,
+            force_focus: false,
+        }, {
+            text:"Write [code]int ac;[/code]",
+            editor:1,
+            editorText: false,
+            complete: function(code, user, output) {
+                var reg = /int ac;/gi;
+                if( reg.exec( code ) ) {
+                    return true;
+                }
+                return false;
+            },
+            lock: false,
+            release: true,
+            force_focus: true,
+            allow_skip: true
+        }, {
+            text: "Skip me!",
+            editor: 0,
+            editorText: "Blank",
+            complete: false,
+            lock: true,
+            force_focus: true
         }
     ]
-
 };
