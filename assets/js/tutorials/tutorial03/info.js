f7ba1f5334a8ec0e7e97a52b4c924addc76f89fd
cbauerman <cbauerman@gmail.com>
2014-05-15 20:23:53 -0700

    Further work on particles

--- assets/js/tutorials/tutorial03/info.js
+++ assets/js/tutorials/tutorial03/info.js
@@ -1,17 +1,17 @@
 var tutorial_data =
 {
-    name:"Vector Operations",
+    name:"Particles",
     steps:
     [
         {
             text:"Welcome to the particles tutorial!",
             editor:1,
-            editorText: '/js/tutorials/tutorial02/desaturate.c',
-            otherText: {'0':'/js/tutorials/tutorial02/desaturate.js', '2' : '/js/tutorials/tutorial02/start.html' },
+            editorText: '/js/tutorials/tutorial03/kernel.cl',
+            otherText: {'0':'/js/tutorials/tutorial03/tut3.js', '2' : '/js/tutorials/tutorial03/start.html' },
             complete: function(code, user, output) {
                 return false;
             },
-            lock: false,
+            lock: true,
             forceFocus: false,
 			allowSkip: true
         }
